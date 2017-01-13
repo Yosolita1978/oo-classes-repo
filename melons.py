19eadd73cb338f4e88a69eee75944ba3595aca05
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-12 17:55:31 -0800

    Completed excercise with OO

--- melons.py
+++ melons.py
@@ -1,4 +1,5 @@
 """This file should have our order classes in it."""
+import random
 
 
 class AbstractMelonOrder(object):
@@ -14,17 +15,43 @@ class AbstractMelonOrder(object):
         self.tax = 0.08
         self.country_code = country_code
 
+    def get_base_price(self):
+        self.base_price = random.randint(5, 10)
+        return self.base_price
+
     def get_total(self):
         """Calculate price."""
 
-        base_price = 5
+        base_price = self.get_base_price()
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
         total = (1 + self.tax) * self.qty * base_price
+        if self.order_type == "international" and self.qty < 10:
+            total = total + 3
         return total
 
     def mark_shipped(self):
         """Set shipped to true."""
 
         self.shipped = True
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """ This is only for the Government orders"""
+
+    def __init__(self, species, qty):
+        super(GovernmentMelonOrder, self).__init__(species, qty)
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        """Set inspection to true."""
+
+        self.passed_inspection = passed
+
+    def get_passed_inspection(self):
+        """Return the inspection."""
+
+        return self.passed_inspection
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
